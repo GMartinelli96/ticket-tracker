39ae236a3d1d0791d1f3f4b080f659b384c61b4e
Giuseppe Martinelli <39916810+GMartinelli96@users.noreply.github.com>
2023-11-21 10:58:49 +0100

    Gestito invio form nuovo ticket

--- app/tickets/new/page.tsx
+++ app/tickets/new/page.tsx
@@ -2,21 +2,53 @@
 
 import React from 'react'
 import { Button, TextField } from '@radix-ui/themes'
+import { useForm, Controller } from "react-hook-form";
+import axios from 'axios';
 
 //Import necessari per il MarkDownEditor
 import "easymde/dist/easymde.min.css";
 import SimpleMDE from "react-simplemde-editor";
+import { useRouter } from 'next/navigation';
 
+interface TicketForm {
+  titolo: string;
+  descrizione: string;
+}
 
 const NewTicketPage = () => {
+  const router = useRouter();
+  const { register, control, handleSubmit } = useForm<TicketForm>();
+
   return (
-    <div className='max-w-xl space-y-3'>
+    <form 
+      className='max-w-xl space-y-3' 
+      onSubmit={handleSubmit(async (data) => {
+        await axios.post('/api/tickets', data)
+
+        //Dopo aver salvato il ticket mando l'utente alla pagina dei ticket
+        router.push('/tickets')
+      })}
+    >
         <TextField.Root>
-          <TextField.Input placeholder='Titolo' />
+          <TextField.Input 
+            placeholder='Titolo' 
+            {...register("titolo", { required: true })}
+          />
         </TextField.Root>    
-        <SimpleMDE placeholder='Descrizione problema' />
+        
+        <Controller 
+          name='descrizione'
+          control={control}
+          render={({ field }) => (
+            <SimpleMDE 
+              placeholder='Descrizione problema' 
+              {...field}
+            />
+          )}
+        />
+
         <Button>Crea nuovo ticket</Button>
-    </div>
+    </form>
   )
 }
 
