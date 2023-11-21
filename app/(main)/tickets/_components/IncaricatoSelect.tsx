399903a6658f145442e39b6bf5c8b4f2a259bd0f
Giuseppe <39916810+GMartinelli96@users.noreply.github.com>
2023-11-21 23:58:36 +0100

    Scaricamento dati con ReactQuery

--- app/(main)/tickets/_components/IncaricatoSelect.tsx
+++ app/(main)/tickets/_components/IncaricatoSelect.tsx
@@ -2,28 +2,32 @@
 
 import { User } from '@prisma/client';
 import { Select } from '@radix-ui/themes'
+import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
 import React, { useEffect } from 'react'
+import { Skeleton } from '@/app/components';
 
 const IncaricatoSelect = () => {
-    const [utenti, setUtenti] = React.useState<User[]>([])
+    const { data: utenti, error, isLoading} = useQuery<User[]>({
+        queryKey: ['utenti'],
+        queryFn: async () =>  axios.get<User[]>('/api/utenti').then((res) => res.data),
+        staleTime: 60 * 1000,    // La lista degli utenti viene cachata e tenuta buona per 60 secondi
+        retry: 3,                // Se la richiesta fallisce, viene ritentata 3 volte
+    })
 
-    useEffect(() => {
-        const fetchUtenti = async () => {
-            const { data } = await axios.get<User[]>('/api/utenti')
-            setUtenti(data)
-        }
-
-        fetchUtenti();
-    }, [])
+    if(isLoading) 
+        return <Skeleton />
+    
+    if(error) 
+        return null;
 
     return (
         <Select.Root>
             <Select.Trigger placeholder='Assegna...' />
             <Select.Content>
                 <Select.Group>
-                    <Select.Label>Suggerimenti</Select.Label>
-                    {utenti.map((utente) => (
+                    <Select.Label>Seleziona incaricato</Select.Label>
+                    {utenti?.map((utente) => (
                         <Select.Item key={utente.id} value={utente.id}>
                             {utente.name}
                         </Select.Item>
