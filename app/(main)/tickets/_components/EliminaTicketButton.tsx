3e427e91d3b5ce6fd206ae1a73751660a7b8f843
Giuseppe Martinelli <39916810+GMartinelli96@users.noreply.github.com>
2023-11-21 15:58:21 +0100

    Mostra dialog conferma elimina ticket

--- app/(main)/tickets/_components/EliminaTicketButton.tsx
+++ app/(main)/tickets/_components/EliminaTicketButton.tsx
@@ -1,14 +1,34 @@
-import { Pencil2Icon, TrashIcon } from '@radix-ui/react-icons'
-import { Button } from '@radix-ui/themes'
-import Link from 'next/link'
-import React from 'react'
+"use client";
+
+import { TrashIcon } from '@radix-ui/react-icons';
+import { AlertDialog, Button, Flex } from '@radix-ui/themes';
+import Link from 'next/link';
 
 const EliminaTicketButton = ( { ticketId }: {ticketId: number} ) => {
   return (
-    <Button color='red'>
-        <TrashIcon />
-        <Link href={`/tickets/${ticketId}/edit`}>Elimina Ticket</Link>
-    </Button>
+    <AlertDialog.Root>
+      <AlertDialog.Trigger>
+        <Button color='red'>
+            <TrashIcon />
+            Elimina Ticket
+        </Button>
+      </AlertDialog.Trigger>
+      <AlertDialog.Content>
+        <AlertDialog.Title>Eliminazione ticket</AlertDialog.Title>
+        <AlertDialog.Description>
+          Sei sicuro di voler eliminare il ticket?<br/>
+          Questa operazione è irreversibile!
+        </AlertDialog.Description>
+        <Flex mt="4" gap="3">
+          <AlertDialog.Cancel>
+            <Button variant='soft' color='gray'>Annulla</Button>
+          </AlertDialog.Cancel>
+          <AlertDialog.Action>
+            <Button color='red'>Elimina ticket</Button>
+          </AlertDialog.Action>
+        </Flex>
+      </AlertDialog.Content>
+    </AlertDialog.Root>
   )
 }
 
